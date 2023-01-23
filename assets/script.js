70a0398db5d442219fe26a2613f7b1a79fa1b931
Jsrra <josephserrato10@gmail.com>
2023-01-22 22:33:06 -0800

    adds HTML and JavaScript files

---
+++ assets/script.js
@@ -0,0 +1,42 @@
+var city = document.querySelector("#floatingInputValue");
+var searchBtn = document.querySelector(".search-Btn")
+var apiKey = 'db145fe4344025343d0b1011a4ff4118'
+
+searchBtn.addEventListener("click", function () {
+
+    console.log(city.value)
+    fetch("https://api.openweathermap.org/data/2.5/forecast?lat={" + lat + "}&lon={" + lon + "}&appid={" + apiKey + "}")
+        // getWeather()
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        });
+})
+// var formSubmitHandler = function (event) {
+//     event.preventDefault();
+
+//     var cityInput = citySearch.value.trim();
+// };
+var lon = 37.3388
+var lat = 121.8853
+var getWeather = function () {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={" + lat + "}&lon={" + lon + "}&appid={" + apiKey + "}";
+
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(response)
+                });
+            }
+        })
+        .catch(function (error) {
+            alert('Unable to connect to openWeatherAPI');
+        });
+};
+// formSubmitHandler.addEventListener('submit', formSubmitHandler); {
+//     console.log("clicked")
+// }
+
