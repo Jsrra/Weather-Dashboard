e0b758a2438ce7476438b2893dfe210e2c0c0907
Jsrra <josephserrato10@gmail.com>
2023-01-24 22:33:29 -0800

    adds functioning API calls

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,71 @@
 var city = document.querySelector("#floatingInputValue");
 var searchBtn = document.querySelector(".search-Btn")
+var cityNameEl = document.querySelector("#city-name")
+var tempEl = document.querySelector("#temp")
+var windEl = document.querySelector("#wind")
+var humidityEl = document.querySelector("#humidity")
+var todaysDate = document.querySelector("#today")
+var currentIcon = document.querySelector("#current-icon")
+
 var apiKey = 'db145fe4344025343d0b1011a4ff4118'
 
 searchBtn.addEventListener("click", function () {
     console.log(city.value)
-    getWeather()
+    getWeather(city.value)
 })
-// var formSubmitHandler = function (event) {
-//     event.preventDefault();
 
-//     var cityInput = citySearch.value.trim();
-// };
-var lon = 121.8853
-var lat = 37.3387
-var getWeather = function () {
-    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+var getWeather = function (city) {
+    var apiUrlCoord = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`
 
-    fetch(apiUrl)
+    fetch(apiUrlCoord)
+        .then(function (data) {
+            if (data.ok) {
+                data.json().then(function (data) {
+                    console.log(data)
+                    cityNameEl.textContent = data.name
+                    tempEl.textContent = "Temp: " + data.main.temp + "°F"
+                    windEl.textContent = "Wind: " + data.wind.speed + " MPH"
+                    humidityEl.textContent = "Humidity: " + data.main.humidity + "%"
+                    var lat = data.coord.lat
+                    var lon = data.coord.lon
+                    forecastFiveDay(lat, lon)
+                    currentIcon.textContent = data.weather.icon
+                });
+            }
+
+        })
+        .catch(function (error) {
+            alert('Unable to connect to openWeatherAPI');
+        });
+
+};
+
+function forecastFiveDay(lat, lon) {
+
+    var fiveDayApiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
+
+    fetch(fiveDayApiUrl)
         .then(function (data) {
             if (data.ok) {
                 data.json().then(function (data) {
                     console.log(data)
                 });
             }
+
         })
-        .catch(function (error) {
-            alert('Unable to connect to openWeatherAPI');
-        });
-};
+
+}
+
+// $("#today").text(dayjs().format("dddd, MMMM D"))
+
+todaysDate.textContent = dayjs().format("(M/D/YYYY)")
+
 // formSubmitHandler.addEventListener('submit', formSubmitHandler); {
 //     console.log("clicked")
 // }
 
+// var formSubmitHandler = function (event) {
+//     event.preventDefault();
+
+//     var cityInput = citySearch.value.trim();
+// };
